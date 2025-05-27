a3c8113ec3878d7eca23974e0fceb6eb4218ea40
Jaspur <jasperkoers@gmail.com>
2025-05-27 22:12:35 +0200

    Update DetailField.vue

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,24 +1,34 @@
 <template>
     <div class="flex border-b border-40">
         <div class="w-1/4 py-4">
-             <h4 class="font-normal text-80">
+            <h4 class="font-normal text-80">
                 {{ field.name }}
             </h4>
         </div>
         <div class="w-3/4 pt-4">
             <component
-                :item="item"
-                :key="index"
-                v-bind="$props"
-                :is="field.detailItemComponent"
+                v-if="isValidComponent"
                 v-for="(item, index) in field.value"
+                :key="index"
+                :is="field.detailItemComponent"
+                :item="item"
+                v-bind="$props"
             />
+            <div v-else class="text-danger">
+                Component <strong>{{ field.detailItemComponent }}</strong> niet gevonden.
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['resource', 'resourceName', 'resourceId', 'field']
+    props: ['resource', 'resourceName', 'resourceId', 'field'],
+
+    computed: {
+        isValidComponent() {
+            return !!this.$options.components?.[this.field.detailItemComponent];
+        }
+    }
 }
 </script>
