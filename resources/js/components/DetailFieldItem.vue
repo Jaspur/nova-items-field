38c062525fc7d4f69298e7ab760c8e8e55e5e493
Jaspur <jasperkoers@gmail.com>
2025-05-27 22:13:12 +0200

    Update DetailFieldItem.vue

--- resources/js/components/DetailFieldItem.vue
+++ resources/js/components/DetailFieldItem.vue
@@ -1,9 +1,21 @@
 <template>
-    <p class="pb-4">{{ item }}</p>
+    <p class="pb-4" v-if="item !== null && item !== undefined">
+        {{ item }}
+    </p>
+    <p class="pb-4 text-80 italic" v-else>
+        Geen waarde opgegeven
+    </p>
 </template>
 
 <script>
 export default {
-    props: ['item', 'resource', 'field']
+    props: {
+        item: {
+            type: [String, Number, Object],
+            default: null,
+        },
+        resource: Object,
+        field: Object,
+    }
 }
 </script>
