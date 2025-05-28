69acdf900c4307736d357f7e6f28d84bbb5b7b68
Jasper Koers <jasper@coding.agency>
2025-05-28 10:08:29 +0200

    Push

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,7 +1,18 @@
 Nova.booting((Vue, router, store) => {
-    Vue.component('index-nova-items-field', require('./components/IndexField').default)
-Vue.component('detail-nova-items-field', require('./components/DetailField').default)
-Vue.component('form-nova-items-field', require('./components/FormField').default)
-Vue.component('detail-nova-items-field-item', require('./components/DetailFieldItem').default)
-
-})
+  Vue.component(
+    "index-nova-items-field",
+    require("./components/IndexField.vue").default
+  );
+  Vue.component(
+    "detail-nova-items-field",
+    require("./components/DetailField.vue").default
+  );
+  Vue.component(
+    "form-nova-items-field",
+    require("./components/FormField.vue").default
+  );
+  Vue.component(
+    "detail-nova-items-field-item",
+    require("./components/DetailFieldItem.vue").default
+  );
+});
